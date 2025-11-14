59a0bd5975e64404882110dbb4aa195fd472d071
abhijit kumar singh <abhijitkumarsingh.cse58@gmail.com>
2025-11-14 16:02:08 +0530

    chore(init): set up next js 16 project with App router and tailwind css

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginPage from "@/features/auth/pages/Login"
+
+const Page = () => {
+  return (
+    <div><LoginPage /></div>
+  )
+}
+
+export default Page
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+import SignUpPage from "@/features/auth/pages/SignUp"
+
+const Page = () => {
+  return (
+    <div>
+        <SignUpPage />
+    </div>
+  )
+}
+
+export default Page
